3e8d03fe5ce3c8d23db78ec9b07e5c4cac9dd03d
何彦霖 <timothy3181@heyanlindeMacBook-Air.local>
2025-02-26 10:41:10 +0800

    First Push

---
+++ src/main/java/com/timothy/learnmod/LearnModClient.java
@@ -0,0 +1,10 @@
+package com.timothy.learnmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearnModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/timothy/learnmod/LearnModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.timothy.learnmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LearnModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
